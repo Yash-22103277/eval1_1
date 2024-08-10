618c2cb723bc2b73cb6f8830b59e2f82e39e0b50
Yash Gautam <22103277@mail.jiit.ac.in>
2024-08-10 10:49:44 +0530

    Add files via upload

---
+++ eval1.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    cout<<"Enter: ";
+    int n;
+    cin>>n;
+
+    int ar[n][n];
+
+    cout<<"Input your "<<n<<"x"<<n<<" Matrix: \n";
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin>>ar[i][j];
+        }
+    }
+
+    int d;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+
+        }
+    }
+
+
+
+
+}
